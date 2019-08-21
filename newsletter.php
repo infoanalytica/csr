8acda43f58ff5e98f455f8e3edd386af7b92d63e
ankitia <ankit.shah@infoanalytica.com>
2019-08-21 14:45:33 +0530

    new letter changes

--- newsletter.php
+++ newsletter.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="icon" type="image/x-icon" href="img/favicon.png">
     <link rel="stylesheet" href="css/csr.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -15,16 +15,30 @@
 <!--Navbar-->
     <?php include "include/header.php" ?>
 
+ <!-- Title -->
+ <section id="association">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-12 areatitle">
+                <h1 class="sec-title">THE IMPRINTS</h1>
+            </div>
+            <div class="col-12 abouttitle">
+                <p class="p-size">The Imprints is our CSR newsletter designed especially for iA employees, our partners, and affiliates. In ‘The Imprints,’ we will try to cover everything that’s happening from the CSR perspective; social and community initiatives, we’re part of, upcoming events, news & announcements, and some insightful trivia. We will also highlight the contributions of our employees and affiliates, who are volunteering and contributing to our initiatives in building equitable and sustainable communities.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
 <!--Cards-->
 <div class="container mt-3 mb-5">
     <div class="row">
-        <div class="col-md-3">
-            <div class="card shadow" style="width: 18rem;">
+        <div class="col-12 d-flex justify-content-center">
+            <div class="card shadow" style="width: 18rem;padding-top:0px;">
                 <img src="img/newsletter_aug.png" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">The Imprints - Aug '19</h5>
-                    <p class="card-text text-justify">The Imprints,’ our CSR newsletter is designed especially for our employees, partners, and affiliates which will give you updates on the latest CSR news, upcoming initiatives and some insightful trivia.</p>
-                    <a href="download/Newsletter_aug.pdf" class="btn btn-primary">Download</a>
+                    <h5 class="card-title">August - 2019</h5>
+                    <p class="p-size text-justify">The Imprints,’ our CSR newsletter is designed especially for our employees, partners, and affiliates which will give you updates on the latest CSR news, upcoming initiatives and some insightful trivia.</p>
+                    <a href="download/Newsletter_aug.pdf" class="btn btn-primary p-size" style="margin-top:0px!important;">Download</a>
                 </div>
             </div>
         </div>
