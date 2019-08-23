ecb05d08ac2e27b86e39913a02e49e622a75abe0
ankitia <ankit.shah@infoanalytica.com>
2019-08-23 17:48:50 +0530

    add google code and robot

--- index.php
+++ index.php
@@ -6,11 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" type="image/x-icon" href="img/favicon.png">
-    <title>Infoanalytica Foundation</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <link rel="stylesheet" href="css/csr.css">
     <link rel="stylesheet" href="css/infinite-slider.css">
+    <title>Corporate Social Responsibility (CSR) – infoAnalytica INC</title>
+    <meta name="description" content="Through iA Foundation, we try to create a culture for change through our efforts in the areas of education, sports, healthcare, heritage & skill development"/>
+    <link rel="canonical" href="http://csr.infoanalytica.com/" />
+
 </head>
 
 <body>
